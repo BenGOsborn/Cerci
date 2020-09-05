837e0e159120097a4712335d1737752e0752540a
BenOsbornn <bengaoosborn@gmail.com>
2020-09-05 22:32:30 +1000

    Update
    Added feedforward algorithm
    Added training algorithm
    Provided sample data to test the network

---
+++ resources.py
@@ -0,0 +1,12 @@
+# I want to create a dot product library
+# I want to create a sigmoid function and a sigmoid derivative
+from math import exp
+
+def dot(arr1, arr2):
+    return sum([one*two for one, two in zip(arr1, arr2)])
+
+def sigmoid(x, deriv=False):
+    if not deriv:
+        return 1/(1+exp(-x))
+        # This is because we pass through the sigmoid value, and therefore the value is not raw and does not need to be parsed as a sigmoid value as it already is one
+    return x*(1-x)
---
+++ single_perceptron.py
@@ -0,0 +1,41 @@
+import resources
+
+class Brain:
+    def __init__(self, weights, bias):
+        self.weights = weights
+        self.bias = bias
+
+    def feedForward(self, inputs):
+        raw = resources.dot(inputs, self.weights) + self.bias
+        return resources.sigmoid(raw)
+
+    def trainNetwork(self, input_data, training_data):
+        # 1/2n*sum(predicted-observed)^2 - One half mean squared error
+        predicted = self.feedForward(input_data)
+
+        error = 0.5*(predicted - training_data) 
+
+        for i in range(len(self.weights)):
+            update = error*resources.sigmoid(predicted, deriv=True)*input_data[i]
+            self.weights[i] -= 0.5*update
+
+        biasUpdate = error*resources.sigmoid(predicted, deriv=True)
+        self.bias -= 0.5*biasUpdate
+
+weights = [0.5, 0.5, 0.5]
+bias = 0.5
+
+inputs = [[0, 0, 0], [0, 0, 1], [1, 0, 0], [1, 0, 1], [1, 1, 1], [0, 1, 0], [0, 1, 1], [1, 0, 0]]
+training_data = [0, 0, 0, 1, 1, 0, 1, 1]
+
+brain = Brain(weights, bias)
+
+for _ in range(10000):
+    for i in range(len(inputs)):
+        brain.trainNetwork(inputs[i], training_data[i])
+
+print(f"Weights: {brain.weights} Biases: {brain.bias}")
+error = 0
+for i in range(len(inputs)):
+    error += (training_data[i] - brain.feedForward(inputs[i]))/len(inputs) 
+print(f"Error: {error}")

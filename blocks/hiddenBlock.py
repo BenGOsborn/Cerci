770584e0b20d5c077445056d211fb9dc9e2ddc19
BenOsbornn <bengaoosborn@gmail.com>
2020-09-06 01:22:49 +1000

    Update
    Created the hiddenBlock

--- blocks/hiddenBlock.py
+++ blocks/hiddenBlock.py
@@ -6,7 +6,30 @@ class HiddenBlock:
         self.bias = bias
 
     def feedForward(self, hidden_inputs):
-        pass
+        output = [
+            sigmoid(
+                dot(hidden_inputs, weights) + bias
+            ) 
+        for weights, bias in zip(self.weights, self.bias)]
+        return output
 
     def train(self, hidden_inputs, hidden_errors):
-        pass
+        error = sum(hidden_errors) / len(hidden_errors)
+        predictions = self.feedForward(hidden_inputs)
+
+        for y in range(len(self.weights)):
+            for x in range(len(self.weights[0])):
+                update = error*sigmoid(predictions[y], deriv=True)*hidden_inputs[x]
+                self.weights[y][x] -= 0.5*update
+
+        for x in range(len(self.weights)):
+            update = error*sigmoid(predictions[x], deriv=True)
+            self.bias[x] -= 0.5*update
+
+        prevErrors = []
+        for y in range(len(self.weights)):
+            for x in range(len(self.weights[0])):
+                prevError = error*sigmoid(predictions[y], deriv=True)*self.weights[y][x]
+                prevErrors.append(prevError)
+        
+        return prevErrors

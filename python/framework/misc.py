fb72d482784da0612a629c0f2b77127e1b0dd43c
BenOsbornn <bengaoosborn@gmail.com>
2020-09-10 15:55:41 +1000

    Update
    Added loss functions mean squared error and crossentropy

--- python/framework/misc.py
+++ python/framework/misc.py
@@ -1,4 +1,4 @@
-from math import exp, tanh
+from math import exp, tanh, log
 from matrix import Matrix, subtract
 
 # Activation functions
@@ -18,13 +18,22 @@ def softmax(val, vals=None, deriv=False):
     return exp(val)/sum([exp(x) for x in vals])
     
 # Loss functions
-
-# Perhaps we should be writing these in terms of the matrices for simplicity
 def meanSquared(predicted_matrix, actual_matrix):
     return subtract(actual_matrix, predicted_matrix)
 
 def crossEntropy(predicted_matrix, actual_matrix):
-    return
+    fn = lambda predicted, actual: -1*(actual/predicted) + (1-actual)/(1-predicted)
+
+    flat_pred = Matrix(arr=predicted_matrix)
+    flat_pred.flatten()
+    flat_act = Matrix(arr=actual_matrix)
+    flat_act.flatten()
+
+    matrix_new = [fn(pred, act) for pred, act in zip(flat_pred.returnMatrix()[0], flat_act.returnMatrix()[0])]
+    ret_matrix = Matrix(matrix_new)
+    ret_matrix.transpose()
+
+    return ret_matrix
 
 # This is the ADAM learning rate paramater which will change depending on the error matrix it receives
 def adam(errors):

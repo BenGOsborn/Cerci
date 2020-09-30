d1cb4f1f6f686ae22e6aa6c119cf4bd28fcc0a88
BenOsborn <bengaoosborn@gmail.com>
2020-09-30 17:00:31 +1000

    Update
    Minor changes

--- cuda/framework/matrix/matrix.cu
+++ cuda/framework/matrix/matrix.cu
@@ -17,27 +17,27 @@ class Matrix {
 		int* shape[2];
 
 		Matrix(float *inMatrix, int *inShape) {
-				// This should point to the same array as the shape parsed in
-				*shape = inShape;
-				*size = inShape[0] * inShape[1];
-				matrix = (float*)malloc(*size);
+			// This should point to the same array as the shape parsed in
+			*shape = inShape;
+			*size = inShape[0] * inShape[1];
+			matrix = (float*)malloc(*size);
 
-				// Can I make use of the GPU to copy the values aswell?
-				float* rMatrix, * wMatrix;
-				cudaMalloc(&rMatrix, size);
-				cudaMalloc(&wMatrix, size);
-				cudaMemcpy(rMatrix, inShape, *size, cudaMemcpyHostToDevice);
+			// Can I make use of the GPU to copy the values aswell?
+			float* rMatrix, * wMatrix;
+			cudaMalloc(&rMatrix, size);
+			cudaMalloc(&wMatrix, size);
+			cudaMemcpy(rMatrix, inShape, *size, cudaMemcpyHostToDevice);
 
-				// Now I just need to specify the block and the thread amounts somehow...?
-				copyMatrix <<< (*size + 255) / 256, 256 >>> (*size, rMatrix, wMatrix);
-				
-				cudaMemcpy(matrix, wMatrix, *size, cudaMemcpyDeviceToHost);
+			// Now I just need to specify the block and the thread amounts somehow...?
+			copyMatrix <<< (*size + 255) / 256, 256 >>> (*size, rMatrix, wMatrix);
+			
+			cudaMemcpy(matrix, wMatrix, *size, cudaMemcpyDeviceToHost);
 
-				cudaFree(rMatrix);
-				cudaFree(wMatrix);
+			cudaFree(rMatrix);
+			cudaFree(wMatrix);
 
-				// Should I also destroy the input matrix here since its already created a copy of it?
-			}
+			// Should I also destroy the input matrix here since its already created a copy of it?
+		}
 };
 
 int main() {

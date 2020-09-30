475261d2b9e52e7956be90069c166ae7897babd5
BenOsborn <bengaoosborn@gmail.com>
2020-09-30 21:00:00 +1000

    Update
    Updated the memory allocation and memory transfer for the matrix object on initialization

--- cuda/framework/matrix/matrix.cu
+++ cuda/framework/matrix/matrix.cu
@@ -1,47 +1,29 @@
 #include <cuda_runtime.h>
 #include <stdlib.h>
-
-// Is it completely necessary to make a brand new copy everytime we want to copy the matrix?
-// Could we just copy the values into the array instead?
-__global__
-void copyMatrix(int N, float *readMatrix, float *writeMatrix) {
-	int i = blockIdx.x * blockDim.x + threadIdx.x;
-	if (i < N) writeMatrix[i] = readMatrix[i];
-}
+#include <string.h>
 
 /// Can I potentially turn this into a tensor class which supports both matrices and tensors in the same class?
 class Matrix {
 	public:
 		float* matrix;
 		int* size;
-		int* shape[2];
+		int* shape;
 
+		// This shape and matrix being parsed seperaretly will probably have to be changed at some point in time or we can just keep it like this?
 		Matrix(float *inMatrix, int *inShape) {
-			// This should point to the same array as the shape parsed in
-			*shape = inShape;
-			*size = inShape[0] * inShape[1];
-			matrix = (float*)malloc(*size);
+			shape = (int*)malloc(2 * sizeof(int));
+			memcpy(shape, inShape, 2*sizeof(int));
 
-			// Can I make use of the GPU to copy the values aswell?
-//			float *rMatrix, *wMatrix;
-//			cudaMalloc(&rMatrix, size);
-//			cudaMalloc(&wMatrix, size);
-//			cudaMemcpy(rMatrix, inShape, *size, cudaMemcpyHostToDevice);
-//
-//			// Now I just need to specify the block and the thread amounts somehow...?
-//			copyMatrix <<< (*size + 255) / 256, 256 >>> (*size, rMatrix, wMatrix);
-//			
-//			cudaMemcpy(matrix, wMatrix, *size, cudaMemcpyDeviceToHost);
-//
-//			cudaFree(rMatrix);
-//			cudaFree(wMatrix);
-//
-//			// Should I also destroy the input matrix here since its already created a copy of it?
+			size = new int(shape[0]*shape[1]);
+
+			matrix = (float*)malloc(*size * sizeof(float));
+			memcpy(matrix, inMatrix, *size * sizeof(float));
 		}
 
 		~Matrix() {
 			free(matrix);
 			free(size);
+			free(shape);
 		}
 };
 
@@ -58,6 +40,5 @@ int main() {
 		vals[i] = 1;
 	}
 
-	//Matrix* matrix = new Matrix(vals, shape);
-
+	Matrix* matrix = new Matrix(vals, shape);
 }

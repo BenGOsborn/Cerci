347dea669d7feed8ca89620c774804faef005fe8
BenOsborn <bengaoosborn@gmail.com>
2020-10-22 09:57:09 +1100

    Update
    Auto commit done by BenOsborn's 'autogit.sh'.

---
+++ python/autograd/autograd.py
@@ -0,0 +1,9 @@
+# Maybe instead I want to setup some sort of graph structure and then go from there
+
+class Node:
+    def __init__(self):
+        pass
+
+class Tree:
+    def __init__(self):
+        pass

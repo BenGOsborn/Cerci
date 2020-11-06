50c754dd92e28ea8072fccf56d0ac839b66c7866
BenOsborn <bengaoosborn@gmail.com>
2020-11-07 00:23:49 +1100

    Removed error checking on base level

--- cerci/cuda/tensoroperations.cu
+++ cerci/cuda/tensoroperations.cu
@@ -5,30 +5,11 @@ void addD(int size, int& ptr1, int& ptr2) {
     int index = blockIdx.x * blockDim.x + threadIdx.x;
 }
 
-std::unique_ptr<float[]> CUDAaddElementwise(std::unique_ptr<float[]>& in_ptr1, std::unique_ptr<int>& in_ptr1_dims, int dim_size1, std::unique_ptr<float[]>& in_ptr2, std::unique_ptr<int>& in_ptr2_dims, int dim_size2) {
+std::unique_ptr<float[]> CUDAaddElementwise(std::unique_ptr<float[]>& in_ptr1, std::unique_ptr<float[]>& in_ptr2, int size) {
     // We could just assume that they are of the same length...?
     // Its cheaper to do the error checking here than to do it on init of each tensor
-
-    if (dim_size1 != dim_size2) throw std::invalid_argument("Pointers are of different sizes!");
-
-    // I could do a try statement here which checks outside of the lengths of the dimsize to see if it can go any further, then if it can returns failure
-    bool valid = false;
-    try {
-        in_ptr1_dims[dim_size1];
-        valid = false;
-        break;
-        in_ptr2_dims[dim_size1];
-        valid = false;
-    } catch (const std::exception& e) { 
-        // This is a bit broken actually
-        throw std::invalid_argument("Sizes do not match the pointer size!");
-    }
-
-    int size;
-    for (int i = 0; i < dim_size1; i++) {
-       if (in_ptr1_dims[i] != in_ptr2_dims[i]) throw std::invalid_argument("Dimensions are of different sizes!"); 
-       size *= in_ptr1_dims[i];
-    }
+    // This assumes that the values we give it are correct as there is no error checking
+    // Remove all checking
 
     int bytes = size * sizeof(float);
 

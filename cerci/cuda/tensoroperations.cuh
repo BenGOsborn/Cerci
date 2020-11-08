7aa1df28e33c10752e21ba28f313396f8ef7861e
BenOsborn <bengaoosborn@gmail.com>
2020-11-08 13:39:53 +1100

    Minor changes

--- cerci/cuda/tensoroperations.cuh
+++ cerci/cuda/tensoroperations.cuh
@@ -5,18 +5,15 @@
 #include <memory>
 #include <cmath>
 
-// Elementwise operations
+// Perform cleanup and standardization of different function
+
 std::unique_ptr<float[]> CUDAaddElementwise(std::unique_ptr<float[]>& in_ptr1, std::unique_ptr<float[]>& in_ptr2, int ptr_size);
 std::unique_ptr<float[]> CUDAmultiplyElementwise(std::unique_ptr<float[]>& in_ptr1, std::unique_ptr<float[]>& in_ptr2, int ptr_size);
 std::unique_ptr<float[]> CUDAdivideElementwise(std::unique_ptr<float[]>& in_ptr1, std::unique_ptr<float[]>& in_ptr2, int ptr_size);
 std::unique_ptr<float[]> CUDAsubtractElementwise(std::unique_ptr<float[]>& in_ptr1, std::unique_ptr<float[]>& in_ptr2, int ptr_size);
 std::unique_ptr<float[]> CUDApowerElementwise(std::unique_ptr<float[]>& in_ptr1, std::unique_ptr<float[]>& in_ptr2, int ptr_size);
 
-// Tricky operations
-
-// In theory we can think of infinite dimensions in terms of just a very long third dimension of different sections
 std::unique_ptr<float[]> CUDAtranspose(std::unique_ptr<float[]>& in_ptr1, std::unique_ptr<int[]>& in_ptr1_dims, int in_ptr1_dims_size, int ptr1_size);
 std::unique_ptr<float[]> CUDAmultiply(std::unique_ptr<float[]>& in_ptr1, std::unique_ptr<int[]>& in_ptr1_dims, std::unique_ptr<float[]>& in_ptr2, std::unique_ptr<int[]>& in_ptr2_dims);
-
-// Apply function down here? Is it necessary? I could just use the tensors into the function and then have the operations applied
-// Use the function and the template I had used before
+// I need a kernel function and a reverse kernel function with strides
+// I need a pooling function with strides

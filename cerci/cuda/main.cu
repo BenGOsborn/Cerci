6936a5908176b2859e2b90576961514ae09fa715
BenOsborn <bengaoosborn@gmail.com>
2020-11-10 14:55:16 +1100

    Tested and confirmed the pooling derivative

--- cerci/cuda/main.cu
+++ cerci/cuda/main.cu
@@ -5,20 +5,25 @@
 
 int main() {
 
-    int size1 = 18;
+    int size1 = 1 << 5;
     int dim_size1 = 3;
-    std::unique_ptr<int[]> dims1(new int[dim_size1]{3, 3, 2});
+    std::unique_ptr<int[]> dims1(new int[dim_size1]{4, 4, 2});
     std::unique_ptr<float[]> mat1(new float[size1]);
     for (int i = 0; i < size1; i++) {
-        mat1[i] = rand() % 10;
+        mat1[i] = rand() % 4;
+    }
+
+    int size2 = 1 << 3;
+    int dim_size2 = 3;
+    std::unique_ptr<int[]> dims2(new int[dim_size1]{2, 2, 2});
+    std::unique_ptr<float[]> mat2(new float[size1]);
+    for (int i = 0; i < size2; i++) {
+        mat2[i] = rand() % 4;
     }
 
     // Nothing is being returned
-    std::unique_ptr<float[]> out_mat = CUDArotate(mat1, dims1, dim_size1, size1);
+    std::unique_ptr<float[]> out_mat = CUDApoolingDeriv(mat1, dims1, dim_size1, size1, mat2, dims2, dim_size2, size2, 2, 2, 2, 2);
 
-    for (int i = 0; i < size1; i++) {
-        std::cout << mat1[i] << " ";
-    }
     std::cout << std::endl;
     for (int i = 0; i < size1; i++) {
         std::cout << out_mat[i] << " ";
